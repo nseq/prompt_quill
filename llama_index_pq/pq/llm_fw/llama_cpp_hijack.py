630a2ac8254629710181adaa27c6302347f86e5a
nseq <30580442+nseq@users.noreply.github.com>
2025-01-26 17:10:08 +0200

    Update llama_cpp_hijack.py

--- llama_index_pq/pq/llm_fw/llama_cpp_hijack.py
+++ llama_index_pq/pq/llm_fw/llama_cpp_hijack.py
@@ -15,16 +15,6 @@
 import sys
 
 try:
-    import llama_cpp
-except:
-    llama_cpp = None
-
-try:
-    import llama_cpp_cuda
-except:
-    llama_cpp_cuda = None
-
-try:
     import llama_cpp_cuda_tensorcores
 except:
     llama_cpp_cuda_tensorcores = None
@@ -33,10 +23,4 @@ except:
 
 class llama_cpp_hijack:
     def __init__(self):
-        if llama_cpp_cuda_tensorcores is not None:
-            sys.modules['llama_cpp'] = llama_cpp_cuda_tensorcores
-        elif llama_cpp_cuda is not None:
-            sys.modules['llama_cpp'] = llama_cpp_cuda
-
-
-
+        sys.modules['llama_cpp'] = llama_cpp_cuda_tensorcores
